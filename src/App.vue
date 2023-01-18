bfa3f30d231264ac62f0c21de39f4ba5ac339008
Asus <emirand20>
2023-01-18 20:12:20 +0100

    vue

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<!-- SFC o Single file componets -> los componentes son una pequeña parte de la aplicacion, 
+los cuales estan metidos y encapsulados en la app: html css y js -->
+<template>
+  <img src='./assets/logo.svg' alt="logo vue" width="125" height="125 "/>
+  <h1>Hola mundo desde Vue.js 3</h1>
+</template>
