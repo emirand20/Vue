3b5f6bc86adacb0c19b2a2c05e973258ec65fd7c
Javier <73743531+emirand20@users.noreply.github.com>
2023-01-19 10:21:20 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <!-- SFC o Single file componets -> los componentes son una pequeña parte de la aplicacion, 
 los cuales estan metidos y encapsulados en la app: html css y js -->
+<!--Un archivo SFC tiene tres partes: el script, la plantilla y el estilo. La sección del script es donde va todo el código JavaScript. 
+El código HTML se colocará en la sección de plantilla . En la sección de estilo , se escribirán los estilos para el HTML.-->
 <template>
   <img src='./assets/logo.svg' alt="logo vue" width="125" height="125 "/>
   <h1>Hola mundo desde Vue.js 3</h1>
