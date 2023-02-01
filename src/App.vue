0a814b0b67ebc9a42399a650bad4d36bd3951a92
Asus <emirand20>
2023-02-01 19:33:18 +0100

    install docker

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <script>
 export default {
   data: () => ({
-    contador: 0
+    contador: 0,
   }),
   methods: {
-    suma(){
-      ++this.contador
+    suma() {
+      ++this.contador;
     },
-    resta(){
-      this.contador--
+    resta() {
+      this.contador--;
     },
-    reset(){
-      this.contador = 0
-    }
-  }
-}
+    reset() {
+      this.contador = 0;
+    },
+  },
+};
 </script>
 <template>
   <div class="container">
@@ -26,22 +26,22 @@ export default {
 
     <h2>Contador {{ contador }}</h2>
     <button @click="suma">+1</button>
-    <button @click="resta">-1</button>
+    <button @click="resta">-1</button>V
     <button @click="reset">reset</button>
   </div>
 </template>
 
 <style scoped>
-small{
-  color: #666
+small {
+  color: #666;
 }
 h1 {
   font-weight: bolder;
 }
-h2{
+h2 {
   font-size: 2.5rem;
 }
-button{
+button {
   cursor: pointer;
   font-size: 1rem;
   margin: 0px 5px;
