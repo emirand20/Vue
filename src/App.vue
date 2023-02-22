f137db561d386a586ffc4cec816895257326430b
Asus <emirand20>
2023-02-22 16:50:16 +0100

    returns

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,33 @@
 <template>
-  <div class="container">
-    <img src=./assets/logo.svg alt="logo vue" width="125" height="125 "/>
-    <small>
-      <h1>Hola mundo desde Vue.js 3</h1>
-    </small>
-    <hr />
+  <h1>Vue</h1>
+  <hr />
+  <!--<span v-text="'Hola mundo'"></span>-->
+  <h2>Tu edad es: {{ edad }}</h2>
+  <h3 v-show="edad >= 18 ">Eres mayor</h3>
+  <span v-show="edad < 18 ">Eres menor</span>
 
-    <h2>Contador {{ contador }}</h2>
-    <button @click="suma">+1</button>
-    <button @click="resta">-1</button>V
-    <button @click="reset">reset</button>
-  </div>
+  <!--<div v-html="html">
+  </div>-->
 </template>
 
 <script>
 export default {
-  data: () => ({
-    contador: 0,
-  }),
-  methods: {
-    suma() {
-      ++this.contador;
-    },
-    resta() {
-      this.contador--;
-    },
-    reset() {
-      this.contador = 0;
-    },
-  },
-};
+  data(){
+    return{
+      edad: 19,
+      //html: '<h3>hola</h3>'
+    }
+  }
+}
 </script>
 
 <style scoped>
-small {
-  color: #666;
-}
-h1 {
-  font-weight: bolder;
-}
-h2 {
-  font-size: 2.5rem;
-}
-button {
-  cursor: pointer;
-  font-size: 1rem;
-  margin: 0px 5px;
+@import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
+
+body {
+  text-align: center;
+  background-color: #4C566A;
+  font-family: 'Lato', sans-serif;
+  color: white;
 }
 </style>
