de1ecbbe18b7cfe43318bcb3dfd38d067bfea9bb
emirand20 <you@jaumebalmes.net>
2023-06-13 16:53:56 +0200

    udemy

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,26 +1,44 @@
 <template>
-    <form>
-        <div class="mb-3">
-            <label for="form-label">Issue</label>
-            <input type="text" class="form-control" placeholder="Enter issue">
-        </div>
-        <div class="mb-3">
-            <label for="form-label">Activity</label>
-            <select>
-                <option disabled value="">Select a type</option>
-                <option value="">Web Aplication with Vue.js</option>
-                <option value="">Backent sevice Node.js</option>
-                <option value="">App Movile with React Native</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="form-label">Issue</label>
-            <input type="text" class="form-control" placeholder="Enter issue">
-        </div>
-        <div class="mb-3">
-            <label class="form-check-label">Urgent </label>
-            <input type="checkbox" form="form-check-input">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <form @submit.prevent="registerProject()">
+    <div class="mb-3">
+      <label for="form-label">Project</label>
+      <input v-model.trim="project" type="text" class="form-control" required />
+    </div>
+    <div class="mb-3">
+      <label for="form-label">Activity</label>
+      <select v-model="type" class="form-select" required>
+        <option disabled selected value="">Select a type</option>
+        <option>Web Aplication with Vue.js</option>
+        <option>Backent sevice Node.js</option>
+        <option>App Movile with React Native</option>
+      </select>
+    </div>
+    <div class="mb-3">
+      <label class="form-check-label"> Urgent </label>
+      <input v-model="checkbox" type="checkbox" form="form-check-input" />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  <hr />
+  <pre>
+        PROJECT: {{ project }} 
+        TYPE: {{ type }} 
+        CHECKBOX: {{ checkbox }}
+    </pre
+  >
 </template>
+
+<script>
+export default {
+  data: () => ({
+    project: "",
+    type: "",
+    checkbox: false,
+  }),
+  methods: {
+    registerProject() {
+      console.log(this.project);
+    },
+  },
+};
+</script>
