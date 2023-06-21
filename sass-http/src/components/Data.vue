77475175786085499ea4da09486dfcf2e5bddd22
Asus <emirand20>
2023-06-21 16:17:46 +0200

    DoneSass

--- sass-http/src/components/Data.vue
+++ sass-http/src/components/Data.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <h2>Data</h2>
+  <div class="data">
+    <h2>Informacion ...</h2>
+    <h3>
+      <span class="cripto">BTC</span> - <span class="moneda">USD</span>
+    </h3>
+    <p>
+      <img src="https://www.cryptocompare.com/media/37746238/eth.png" alt="">
+      Precio: <strong>$300</strong>
+    </p>
   </div>
 </template>
 
